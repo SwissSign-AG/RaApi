fda8d1cf47155219bd78c3bb7edb5e374a80d0d0
SwissSign AG <106153165+opensource-swisssign@users.noreply.github.com>
2022-09-09 09:27:35 +0000

    RA API Auto Update Pipeline 2.0.207-1455465

--- python/swisssign_ra_api/v2/configuration.py
+++ python/swisssign_ra_api/v2/configuration.py
@@ -410,7 +410,7 @@ conf = swisssign_ra_api.v2.Configuration(
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.0.209\n"\
-               "SDK Package Version: 2.0.0".\
+               "SDK Package Version: 1.0.0".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self):
