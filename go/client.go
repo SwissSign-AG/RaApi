afe9092105883de1f95db3c479042141869adaec
SwissSign AG <106153165+opensource-swisssign@users.noreply.github.com>
2023-10-26 11:54:10 +0000

    RA API Auto Update Pipeline 2.3.7-1899767

--- go/client.go
+++ go/client.go
@@ -3,7 +3,7 @@ SwissSign RA REST API
 
 See https://github.com/SwissSign-AG/RaApi/README.md
 
-API version: 2.3.6
+API version: 2.3.7
 Contact: ssc@swisssign.com
 */
 
@@ -42,7 +42,7 @@ var (
 	xmlCheck  = regexp.MustCompile(`(?i:(?:application|text)/xml)`)
 )
 
-// APIClient manages communication with the SwissSign RA REST API API v2.3.6
+// APIClient manages communication with the SwissSign RA REST API API v2.3.7
 // In most cases there should be only one, shared, APIClient.
 type APIClient struct {
 	cfg    *Configuration
